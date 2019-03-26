663a252c5024bde6cdf85c770b19834b7399ef4a
qzy0629 <1015004506@qq.com>
2019-03-26 09:24:56 +0800

    second commit

--- src/main/java/pers/quzhiyu/graduationproject/config/BrowserSecurityConfig.java
+++ src/main/java/pers/quzhiyu/graduationproject/config/BrowserSecurityConfig.java
@@ -1,17 +1,29 @@
-package pers.quzhiyu.graduationproject.config.browser;
+package pers.quzhiyu.graduationproject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/authentication/form")
                 .and()
                 .authorizeRequests()
+                .antMatchers("/login.html").permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().csrf().disable();
     }
 }
